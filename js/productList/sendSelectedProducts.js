cb782b168469e5ce1ab88b6396ada59c1d5fff47
muko <m_vadachkoria@cu.edu.ge>
2021-03-06 21:49:16 +0400

    corrected POST method

--- js/productList/sendSelectedProducts.js
+++ js/productList/sendSelectedProducts.js
@@ -1,37 +1,29 @@
 const sku = new Array();
 
-
 const deleteButton = document.querySelector(".btn-danger");
 
+deleteButton.addEventListener("click", () => {
+  if (sku.length === 0) {
+    alert("select at least on item");
+    return false;
+  }
 
-deleteButton.addEventListener('click', () => {
-
-    if(sku.length === 0) {
-        alert('select at least on item');
-        return false;
-    }
-
-    fetch("PHP/deleteItem.php", {
-        method: "post",
-        mode: "same-origin",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json"
-            },
-        body: JSON.stringify(sku),
-    })
-    .then( function (data) {
-        return data.json();
-        
+  fetch("PHP/deleteItem.php", {
+    method: "POST",
+    mode: "same-origin",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sku),
+  })
+    .then(function (data) {
+      return data.json();
     })
     .then(function (data) {
-        getItemstoProductlist(data);
+      getItemstoProductlist(data);
     })
     .catch(function (error) {
-        console.error(error);
+      console.error(error);
     });
-})
-
-
-
-
+});
